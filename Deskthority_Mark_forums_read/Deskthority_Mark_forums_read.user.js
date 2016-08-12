b2122afab681dd4cb83783aa113a06e4f601ad11
Bruno Sabença <brunosabenca@users.noreply.github.com>
2016-08-12 14:32:59 +0100

    Adds my first script

---
+++ Deskthority_Mark_forums_read/Deskthority_Mark_forums_read.user.js
@@ -0,0 +1,45 @@
+﻿// ==UserScript==
+// @name        Deskthority: Mark forums read
+// @author      Bruno Sabença
+// @version     1
+// @description Mark all forums read from the unread posts page
+// @namespace   https://deskthority.net
+// @include     https://deskthority.net/search.php?search_id=unreadposts
+// @include     https://deskthority.net/?markread
+// @require     https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js
+// @grant       GM_addStyle
+// ==/UserScript==
+
+this.$ = this.jQuery = jQuery.noConflict(true);
+
+$(document).ready(function() {
+    if (window.location.href == "https://deskthority.net/search.php?search_id=unreadposts") {
+        var markread_link_html = "<span class=\"markread\">Mark forums read</span> • ";
+
+        $(".rightside.pagination").each(function() {
+            $(this).prepend(markread_link_html);
+        });
+
+        $(".markread").each(function() {
+            $(this).click(function() {
+                window.location.href = "https://deskthority.net?markread";
+            });
+        });
+    }
+
+    if (window.location.href == "https://deskthority.net/?markread") {
+        var mark_unread_href = $("a:contains('Mark forums read')").attr("href");
+
+        window.location.replace(mark_unread_href);
+    }
+});
+
+GM_addStyle("                   \
+.markread {                     \
+cursor: pointer;                \
+cursor: hand;                   \
+}                               \
+.markread:hover {               \
+text-decoration: underline;     \
+}                               \
+");
